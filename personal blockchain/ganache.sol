4c9086365f64a6851751763fa5befa3fa1a68f13
gab0071 <jeicarm7@gmail.com>
2022-11-16 16:50:42 -0500

    📚 Personal Blockchain

---
+++ personal blockchain/ganache.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/* 
+
+(ganache GUI) HTTP://0.0.0.0:7545 
+(ganache CLI) HTTP://0.0.0.0:8545 
+
+*/
+
+contract ganache {
+
+    // Direccion del propietario del Smart Contract
+    address public owner;
+
+    // Direccion del Smart Contract
+    address public smartContract;
+
+    // Declaracion del mensaje
+    string message = "";
+
+    // Evento de construccion del Smart Contract
+    event newContract(address owner, address smartContract);
+
+    // Evento de publicacion del mensaje
+    event newMessage(string message);
+
+    // Constructor 
+    constructor(){
+        owner = msg.sender;
+        smartContract = address(this);
+        emit newContract(owner, smartContract);
+    }
+
+    // Publicar un mensaje a la Blockchain
+    function setMessage(string memory _message) public {
+        message = _message;
+        emit newMessage(message);
+    }
+
+    // Visualizar el mensaje de la Blockchain
+    function getMessage() public view returns (string memory){
+        return message;
+    }
+
+}
